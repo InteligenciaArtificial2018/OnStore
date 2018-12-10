42ed509b0ca798b849eed4426a7747a8a842b817
Cesar Romero <cesarroh04@hotmail.com>
2018-12-09 21:46:24 -0600

    Corrección tabla Categorias

--- app/src/main/java/tech/ecsoftware/onstore/data/Categorias.kt
+++ app/src/main/java/tech/ecsoftware/onstore/data/Categorias.kt
@@ -4,7 +4,7 @@ import android.arch.persistence.room.ColumnInfo
 import android.arch.persistence.room.Entity
 import android.arch.persistence.room.PrimaryKey
 
-@Entity(tableName = "Productos")
+@Entity(tableName = "Categorias")
 class Categorias(
         @ColumnInfo(name  = "Nombre del categoria")
         var nombreCategoria: String = "",
