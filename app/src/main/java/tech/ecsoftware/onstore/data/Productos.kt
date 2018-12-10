e4be59ad46e671bc14313f99e9af1567c0698749
Cesar Romero <cesarroh04@hotmail.com>
2018-12-09 21:29:57 -0600

    Creación de tabla Productos

---
+++ app/src/main/java/tech/ecsoftware/onstore/data/Productos.kt
@@ -0,0 +1,22 @@
+package tech.ecsoftware.onstore.data
+
+import android.arch.persistence.room.ColumnInfo
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.PrimaryKey
+
+@Entity(tableName = "Productos")
+class Productos(
+        @ColumnInfo(name = "Nombre del producto")
+        var nombreProducto: String = "",
+        @ColumnInfo(name = "Categoria")
+        var categoriaProducto: String = "",
+        @ColumnInfo(name = "Cantidad en inventario")
+        var cantidadProducto: String = "",
+        @ColumnInfo(name = "Descripción")
+        var descripcionProducto: String = "",
+        @ColumnInfo(name = "Precio")
+        var precioProdcuto: Double = 0.00) {
+
+        @PrimaryKey(autoGenerate = false)
+        var idProducto: String = ""
+}
