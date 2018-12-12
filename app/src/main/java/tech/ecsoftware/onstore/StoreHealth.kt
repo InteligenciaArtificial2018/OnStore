23cc8570aae7f452570c5e24a73031628781c8bf
Cesar Romero <cesarroh04@hotmail.com>
2018-12-11 23:16:59 -0600

    Implementación de recycle view en las pantallas de las categorías de la tienda

---
+++ app/src/main/java/tech/ecsoftware/onstore/StoreHealth.kt
@@ -0,0 +1,12 @@
+package tech.ecsoftware.onstore
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class StoreHealth : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_health)
+    }
+}
